39cf2985cc1959622c3e505445b1333006de372c
Wilrosmi <will.r.smith31@gmail.com>
2022-07-13 17:56:37 +0100

    Remove redundant words in img alt

--- src/Entry.tsx
+++ src/Entry.tsx
@@ -13,7 +13,7 @@ function Entry(props: EntryStructure): JSX.Element {
       <h2>{props.title}</h2>
       <h3>{props.name}</h3>
       <h3>{props.country}</h3>
-      <img src="{props.image}" alt="A photo of {props.name}" />
+      <img src="{props.image}" alt="{props.name}" />
       <a href="{props.map}"> Map Link</a>
       <p>{props.description}</p>
     </>
