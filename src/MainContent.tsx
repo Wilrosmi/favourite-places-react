c881e1b2297d7d24fccbcaddd0bce09f8028d4c4
Wilrosmi <will.r.smith31@gmail.com>
2022-07-13 16:44:19 +0100

    Remove unneeded interface and props for MainContent

--- src/MainContent.tsx
+++ src/MainContent.tsx
@@ -1,15 +1,6 @@
 import Entry from "./Entry";
 
-interface EntryStructure {
-    title: string;
-    name: string;
-    country: string;
-    image: URL;
-    map: URL;
-    description: string;
-}
-
-function MainContent(props: EntryStructure): JSX.Element {
+function MainContent(): JSX.Element {
     return (
         <>
             <Entry />
